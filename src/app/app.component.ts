837d0965368af04aae8916742cb82aa017a4811b
computernerdzach <wizteczach@gmail.com>
2023-06-15 10:19:09 -0700

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,22 @@
 import { Component } from '@angular/core';
 import { type } from 'os';
 
-interface Pokemon {
-  id: number,
-  name: string,
-  type: string
-  isCool: boolean,
-  isStylish: boolean
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  pokemons: Pokemon[] = [{
-    id: 1,
-    name: 'pikachu',
-    type: 'electric',
-    isCool: false,
-    isStylish: true
-  }, {
-    id: 2,
-    name: 'squirtle',
-    type: 'water',
-    isCool: true,
-    isStylish: true
-  }, {
-    id: 3,
-    name: 'charmander',
-    type: 'fire',
-    isCool: true,
-    isStylish: false
-  }];
+  
+  pokemonName: string = "";
+  albert: string = "";
 
   constructor() {
     
   }
 
-  handleClick(value: any) {
-    console.log(value);
+  handleChange(event: any) {
+    this.pokemonName = event.target.value;
   }
 
 }
