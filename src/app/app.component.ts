5c5131c80b97e9607e8c0bc0dea52d03a663cb78
computernerdzach <wizteczach@gmail.com>
2023-06-16 11:30:15 -0700

    angular modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ interface Pokemon {
   id: number,
   name: string,
   type: string,
-  isCool: boolean
+  isCool: boolean,
+  isStylish: boolean
 }
 
 @Component({
@@ -19,17 +20,20 @@ export class AppComponent {
     id: 1,
     name: 'pikachu',
     type: 'electric',
-    isCool: true
+    isCool: true,
+    isStylish: true
   }, {
     id: 2,
     name: 'squirtle',
     type: 'water',
-    isCool: true
+    isCool: true,
+    isStylish: false
   }, {
     id: 3,
     name: 'charmander',
     type: 'fire',
-    isCool: false
+    isCool: false,
+    isStylish: false
   }]
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonBaseModule } from './pokemon-base/pokemon-base.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    PokemonBaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
